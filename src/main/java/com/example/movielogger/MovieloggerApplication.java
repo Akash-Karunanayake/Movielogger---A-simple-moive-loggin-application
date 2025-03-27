0c72570d0d1ed3686fb6db882d3ae0e01ed32399
Akash Karunanayake <akashwishvajith@gmail.com>
2025-03-27 11:29:58 +0530

    Initial commit

---
+++ src/main/java/com/example/movielogger/MovieloggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.movielogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movielogger/MovieloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movielogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
